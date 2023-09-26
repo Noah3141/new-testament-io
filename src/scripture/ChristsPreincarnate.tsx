ebe0b1f280d811604b94fbd8d8c623fb013a8e8e
Noah Steckley <Noah3141@gmail.com>
2023-09-26 15:17:30 -0500

    oop

--- src/scripture/ChristsPreincarnate.tsx
+++ src/scripture/ChristsPreincarnate.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import Notebook from "~/components/Layouts/Notebook";
-import Scripture from "~/components/Scripture";
+// import React from "react";
+// import Notebook from "~/components/Layouts/Notebook";
+// import Scripture from "~/components/Scripture";
 
-const Index = () => {
-    const verse;
-    const title;
-    const kjv;
-    const lexham;
-    const mounce = "";
+// const Index = () => {
+//     const verse;
+//     const title;
+//     const kjv;
+//     const lexham;
+//     const mounce = "";
 
-    const id = "a patch of unshrunk cloth on an old garment";
-    return (
-        <Notebook>
-            <Scripture
-                title={title}
-                verse={verse}
-                lexhamText={lexham}
-                kjvText={kjv}
-                mounceText={mounce}
-                scriptureId={id}
-            />
-        </Notebook>
-    );
-};
+//     const id = "a patch of unshrunk cloth on an old garment";
+//     return (
+//         <Notebook>
+//             <Scripture
+//                 title={title}
+//                 verse={verse}
+//                 lexhamText={lexham}
+//                 kjvText={kjv}
+//                 mounceText={mounce}
+//                 scriptureId={id}
+//             />
+//         </Notebook>
+//     );
+// };
 
-export default Index;
+// export default Index;
