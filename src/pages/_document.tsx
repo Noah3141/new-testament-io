258e7882590ca8b955bc2d9dd17cd8c2fcfd1309
Noah Steckley <Noah3141@gmail.com>
2023-09-29 10:06:30 -0500

    Public presentation fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <title>New Testament Notebook</title>
+                <meta
+                    name="description"
+                    content="Create, save, compare interpretations of texts"
+                />
+                <link rel="icon" href="/favicon.ico" />{" "}
+            </Head>
             <body className="bg-basic-900 text-primary-500">
                 <Main />
                 <NextScript />
