8b9008ff651f1041886ea8daa376bb3f2a09655e
Noah Steckley <Noah3141@gmail.com>
2023-09-29 10:06:48 -0500

    no empty Head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ export default function Home() {
 
     return (
         <>
-            <Head></Head>
             <main className=" flex min-h-screen flex-col items-center justify-center"></main>
         </>
     );
